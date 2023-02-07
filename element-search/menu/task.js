c6256f63f047bfb977a3570989bf9be553cfcc84
Aleks Khukh <aleks.khukh@gmail.com>
2023-02-07 12:59:27 +0300

    Новая версия задание Меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,12 +2,14 @@ const menuSub = [...document.querySelectorAll('.menu_sub')];
 
 for (let index in menuSub) {
 
-  let menuSubUrl = menuSub[index].closest('.menu__item');
+  let menuSubActiv = menuSub[index].closest('.menu__item');
+  let menuSubURL = menuSubActiv.querySelector('a');
 
-  menuSubUrl.onclick = () => {
+  menuSubURL.onclick = () => {
 
     if (menuSub[index].classList.contains('menu_active')) {
       menuSub[index].classList.remove('menu_active');
+      return false;
     } else {
       for (let i in menuSub) {
         if (menuSub[i].classList.contains('menu_active')) {
@@ -15,9 +17,9 @@ for (let index in menuSub) {
         }
       }
       menuSub[index].classList.add('menu_active');
+      return false;
     }
 
-    return false;
   }
   
 }
